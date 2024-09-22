ef164566a67e13e103e31b4f258ef0273fdde478
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-09-22 23:10:46 +0200

    chore: set custom application properties for consumers

--- kafka-consumer/src/main/java/com/kcm/msp/dev/app2/development/prototype/kafka/consumer/service/KafkaConsumerService.java
+++ kafka-consumer/src/main/java/com/kcm/msp/dev/app2/development/prototype/kafka/consumer/service/KafkaConsumerService.java
@@ -14,16 +14,16 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumerService {
 
   @KafkaListener(
-      topics = {"test_string-topic"},
-      groupId = "test_string-group1")
+      topics = {"${consumers.string_consumer.topics}"},
+      groupId = "${consumers.string_consumer.groupId}")
   public void consumeString(@Payload final String message) {
     log.info("String message: {}", message);
   }
 
   @KafkaListener(
-      topics = {"test_string-topic"},
-      groupId = "test_batch_string-group1",
-      containerFactory = "batchContainerFactory")
+      topics = {"${consumers.batch_string_consumer.topics}"},
+      groupId = "${consumers.batch_string_consumer.groupId}",
+      containerFactory = "${consumers.batch_string_consumer.containerFactory}")
   public void consumeBatchString(@Payload final List<String> messages) {
     for (final String message : messages) {
       log.info("[Batch]String message: {}", message);
@@ -31,15 +31,14 @@ public class KafkaConsumerService {
   }
 
   @KafkaListener(
-      topics = {"test_message_obj-topic"},
-      groupId = "test_message_obj-group1",
-      containerFactory = "messageContainerFactory")
+      topics = {"${consumers.message_consumer.topics}"},
+      groupId = "${consumers.message_consumer.groupId}",
+      containerFactory = "${consumers.message_consumer.containerFactory}")
   public void consumeMessage(@Payload final Message message, final Acknowledgment acknowledgment) {
     CompletableFuture.runAsync(() -> log.info("Object message: {}", message))
         .thenRun(acknowledgment::acknowledge) // Acknowledge the message after successful processing
         .exceptionally(
             ex -> {
-              // Handle failure (e.g., retry or send to DLQ)
               log.error("Error in processing message: {}", message, ex);
               return null;
             });

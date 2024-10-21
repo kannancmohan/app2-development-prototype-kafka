7e51f272ccc578f84cd03c18dc6294e55558c739
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-10-21 16:25:47 +0200

    chore: update test for ssl kafka

--- kafka-consumer/src/test/java/com/kcm/msp/dev/app2/development/prototype/kafka/consumer/KafkaConsumerWithSSLAndBearerAuthIntegrationTest.java
+++ kafka-consumer/src/test/java/com/kcm/msp/dev/app2/development/prototype/kafka/consumer/KafkaConsumerWithSSLAndBearerAuthIntegrationTest.java
@@ -3,32 +3,33 @@ package com.kcm.msp.dev.app2.development.prototype.kafka.consumer;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import java.time.Duration;
+import java.util.List;
 import java.util.stream.StreamSupport;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.DisabledIf;
 
-@ActiveProfiles({"test", "test-ssl-and-bearer-auth"})
+@ActiveProfiles({"test-ssl-and-bearer-auth"})
 @Tag("IntegrationTest")
 @DisabledIf(expression = "#{environment['skip.integration.test'] == 'true'}")
+@ContextConfiguration(initializers = {WireMockInitializer.class})
 @SpringBootTest(
     properties = {
       "OAUTH2_CLIENT_ID="
@@ -41,91 +42,51 @@ import org.springframework.test.context.junit.jupiter.DisabledIf;
           + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.CONSUMER_OAUTH_CLIENT_SCOPE,
       "spring.kafka.bootstrap-servers=localhost:"
           + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_SSL_PORT
-    })
-@EmbeddedKafka(
-    partitions = 2,
-    brokerProperties = {
-      "listeners=PLAINTEXT://:"
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_PORT
-          + ",SASL_SSL://:"
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_SSL_PORT,
-      "listener.security.protocol.map = PLAINTEXT:PLAINTEXT,SASL_SSL:SASL_SSL",
-      "ssl.keystore.location="
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_KEYSTORE_LOCATION,
-      "ssl.keystore.password="
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_KEYSTORE_PWD,
-      "ssl.truststore.location="
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_TRUSTSTORE_LOCATION,
-      "ssl.truststore.password="
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_TRUSTSTORE_PWD,
-      "ssl.keystore.type=pkcs12",
-      "ssl.truststore.type=pkcs12",
-      "ssl.endpoint.identification.algorithm=",
-      "sasl.enabled.mechanisms=PLAIN, OAUTHBEARER",
-      "sasl.mechanism.inter.broker.protocol=PLAIN",
-      "listener.name.sasl_ssl.sasl.enabled.mechanisms=OAUTHBEARER",
-      "listener.name.sasl_ssl.oauthbearer.sasl.jaas.config=org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule"
-          + " required;",
-      "listener.name.sasl_ssl.oauthbearer.sasl.server.callback.handler.class=org.apache.kafka.common.security.oauthbearer.OAuthBearerValidatorCallbackHandler",
-      "sasl.oauthbearer.jwks.endpoint.url="
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_SASL_JWKS_ENDPOINT,
-      "sasl.oauthbearer.expected.audience="
-          + KafkaConsumerWithSSLAndBearerAuthIntegrationTest.BROKER_SASL_EXPECTED_AUD
-    },
-    topics = {
-      KafkaConsumerWithSSLAndBearerAuthIntegrationTest.STRING_TOPIC,
-      KafkaConsumerWithSSLAndBearerAuthIntegrationTest.MESSAGE_TOPIC
+      // "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
     })
 public class KafkaConsumerWithSSLAndBearerAuthIntegrationTest {
-
-  public static final String BROKER_PORT = "19881";
   public static final String BROKER_SSL_PORT = "19882";
-
-  public static final String BROKER_KEYSTORE_LOCATION =
-      "src/test/resources/self-signed-certs/server-keystore.p12";
-  public static final String BROKER_KEYSTORE_PWD = "test@test.com";
-
-  public static final String BROKER_TRUSTSTORE_LOCATION =
-      "src/test/resources/self-signed-certs/server-truststore.p12";
-  public static final String BROKER_TRUSTSTORE_PWD = "test@test.com";
-  public static final String BROKER_SASL_JWKS_ENDPOINT =
-      "http://192.168.0.30:8666/realms/homelab/protocol/openid-connect/certs";
-  public static final String BROKER_SASL_EXPECTED_AUD = "account";
 
   public static final String STRING_TOPIC = "int_test_ssl_bearer_string-topic";
   public static final String MESSAGE_TOPIC = "int_test_ssl_bearer_message_obj-topic";
-
   public static final String CONSUMER_OAUTH_CLIENT_ID = "kafka-consumer2";
   public static final String CONSUMER_OAUTH_CLIENT_SECRET = "ZX10LkmMA8iESde9AjpL5hnJZbQPXzzm";
   public static final String CONSUMER_OAUTH_TOKEN_ENDPOINT =
-      "http://192.168.0.30:8666/realms/homelab/protocol/openid-connect/token";
+      "http://localhost:19883/mock-idp/protocol/openid-connect/token";
   public static final String CONSUMER_OAUTH_CLIENT_SCOPE = "openid profile";
 
-  @Autowired private EmbeddedKafkaBroker broker;
+  @Autowired private EmbeddedKafkaBroker embeddedKafka;
   @Autowired private KafkaProperties kafkaProperties;
+
+  @Autowired private AdminClient adminClient;
 
   @Autowired
   private ConcurrentKafkaListenerContainerFactory<String, String> defaultContainerFactory;
 
+  @Autowired WireMockServer wireMockServer;
+
   @Nested
-  @TestInstance(PER_CLASS)
+  // @TestInstance(PER_CLASS)
   class TestStingPayload {
 
     private Producer<String, String> producer;
 
-    @BeforeAll
+    /*    @BeforeAll
     void beforeAll() {
-      final var producerProperties = kafkaProperties.buildProducerProperties(null);
-      // final var producerProperties = KafkaTestUtils.producerProps(broker);
+      //final var producerProperties = kafkaProperties.buildProducerProperties(null);
+       final var producerProperties = KafkaTestUtils.producerProps(embeddedKafka);
       producer = new KafkaProducer<>(producerProperties);
-    }
+    }*/
 
     @Test
     void stringMessageShouldInvokeKafkaConsumer() {
       final var payloadKey = "test_string-key";
       final var payload = "Sending with our own simple KafkaProducer";
       final var group = "test_string-group1";
+      final var producerProperties = kafkaProperties.buildProducerProperties(null);
+      producer = new KafkaProducer<>(producerProperties);
       producer.send(new ProducerRecord<>(STRING_TOPIC, payloadKey, payload));
+
       final var consumer = getConsumer(defaultContainerFactory, STRING_TOPIC, group);
       final var records = KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(30));
       assertAll(
@@ -145,7 +106,8 @@ public class KafkaConsumerWithSSLAndBearerAuthIntegrationTest {
       final String topicId,
       final String groupId) {
     final var consumer = containerFactory.getConsumerFactory().createConsumer(groupId, "clientId");
-    broker.consumeFromAnEmbeddedTopic(consumer, topicId);
+    consumer.subscribe(List.of(topicId));
+    // embeddedKafka.consumeFromAnEmbeddedTopic(consumer, topicId);
     return (Consumer<K, V>) consumer;
   }
 }
--- kafka-consumer/src/test/java/com/kcm/msp/dev/app2/development/prototype/kafka/consumer/WireMockInitializer.java
+++ kafka-consumer/src/test/java/com/kcm/msp/dev/app2/development/prototype/kafka/consumer/WireMockInitializer.java
@@ -11,14 +11,24 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.jackson.io.JacksonSerializer;
+import io.jsonwebtoken.security.Jwk;
+import io.jsonwebtoken.security.JwkSet;
+import io.jsonwebtoken.security.Jwks;
 import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -85,11 +95,14 @@ public class WireMockInitializer
       .of("github.url:http://localhost:" + wireMockServer.port())
       .applyTo(applicationContext);*/
       final var embeddedKafka = generateKafkaBroker();
+      //      embeddedKafka.afterPropertiesSet();
       applicationContext.getBeanFactory().registerSingleton("embeddedKafka", embeddedKafka);
+
       KafkaAdmin kafkaAdmin = kafkaAdmin(embeddedKafka);
-      applicationContext.getBeanFactory().registerSingleton("kafkaAdmin", kafkaAdmin);
+      // applicationContext.getBeanFactory().registerSingleton("kafkaAdmin", kafkaAdmin);
+
       AdminClient adminClient = adminClient(kafkaAdmin);
-      applicationContext.getBeanFactory().registerSingleton("adminClient", adminClient);
+      applicationContext.getBeanFactory().registerSingleton("kafkaAdminClient", adminClient);
       embeddedKafka.afterPropertiesSet();
     } catch (Exception e) {
       throw new RuntimeException(e);
@@ -98,10 +111,35 @@ public class WireMockInitializer
 
   private static WireMockServer generateWireMockServer() throws Exception {
 
-    String mockTokenResponse =
-        "{ \"access_token\": \""
-            + generateToken(CONSUMER_OAUTH_CLIENT_ID, BROKER_SASL_EXPECTED_AUD)
-            + "\" }";
+    final String kid = UUID.randomUUID().toString();
+    final KeyPair keyPair = JwtUtil.generateEncodedKeyPair();
+    final String jwks_json_old = JwtUtil.generateJwks(keyPair.getPublic(), kid);
+
+    // TEST//
+    KeyPair pair = Jwts.SIG.RS512.keyPair().build();
+    RSAPublicKey pubKey = (RSAPublicKey) pair.getPublic();
+    RSAPrivateKey privKey = (RSAPrivateKey) pair.getPrivate();
+
+    /*RsaPublicJwk jwk = Jwks.builder().key(pubKey).idFromThumbprint().build();
+    byte[] utf8Bytes = new JacksonSerializer().serialize(jwk);
+    String jwks_json = new String(utf8Bytes, StandardCharsets.UTF_8);*/
+
+    Jwk jwk = Jwks.builder().id("key-id-123").key(pubKey).algorithm("RS512").build();
+    JwkSet set = Jwks.set().add(jwk).build();
+    byte[] utf8Bytes = new JacksonSerializer().serialize(set);
+    String jwks_json = new String(utf8Bytes, StandardCharsets.UTF_8);
+
+    final String access_token =
+        JwtUtil.generateRsaJwt(
+            privKey, kid, CONSUMER_OAUTH_CLIENT_ID, Map.of("aud", BROKER_SASL_EXPECTED_AUD), 300);
+    /*    final String access_token =
+    JwtUtil.generateJwt(
+        keyPair.getPrivate(),
+        kid,
+        CONSUMER_OAUTH_CLIENT_ID,
+        Map.of("aud", BROKER_SASL_EXPECTED_AUD),
+        300);*/
+    final String access_token_json = "{ \"access_token\": \"" + access_token + "\" }";
 
     WireMockServer wireMockServer =
         new WireMockServer(WireMockConfiguration.wireMockConfig().port(19883));
@@ -113,7 +151,7 @@ public class WireMockInitializer
                 aResponse()
                     .withStatus(OK.value())
                     .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                    .withBody(MOCK_JWKS_RESPONSE)));
+                    .withBody(jwks_json)));
 
     wireMockServer.stubFor(
         post(urlEqualTo("/mock-idp/protocol/openid-connect/token"))
@@ -122,7 +160,7 @@ public class WireMockInitializer
                 aResponse()
                     .withStatus(OK.value())
                     .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                    .withBody(mockTokenResponse)));
+                    .withBody(access_token_json)));
     return wireMockServer;
   }
 
@@ -145,21 +183,21 @@ public class WireMockInitializer
     return Keys.hmacShaKeyFor(keyBytes);
   }
 
-  private static EmbeddedKafkaBroker generateKafkaBroker() {
+  private static EmbeddedKafkaBroker generateKafkaBroker() throws Exception {
     final var listeners =
+        "INTERNAL://:"
+            + WireMockInitializer.BROKER_PORT
+            + ",EXTERNAL://:"
+            + WireMockInitializer.BROKER_SSL_PORT;
+    final var advListeners =
         "INTERNAL://localhost:"
             + WireMockInitializer.BROKER_PORT
             + ",EXTERNAL://localhost:"
             + WireMockInitializer.BROKER_SSL_PORT;
-    final var advListeners =
-        "INTERNAL://127.0.0.1:"
-            + WireMockInitializer.BROKER_PORT
-            + ",EXTERNAL://127.0.0.1:"
-            + WireMockInitializer.BROKER_SSL_PORT;
     final var brokerProperties = new HashMap<String, String>();
     // brokerProperties.put("security.protocol","SASL_SSL");
     brokerProperties.put("listeners", listeners);
-    // brokerProperties.put("advertised.listeners", advListeners);
+    brokerProperties.put("advertised.listeners", advListeners);
     brokerProperties.put("listener.security.protocol.map", "INTERNAL:PLAINTEXT,EXTERNAL:SASL_SSL");
     brokerProperties.put("ssl.keystore.location", WireMockInitializer.BROKER_KEYSTORE_LOCATION);
     brokerProperties.put("ssl.keystore.password", WireMockInitializer.BROKER_KEYSTORE_PWD);
@@ -170,6 +208,8 @@ public class WireMockInitializer
     brokerProperties.put("ssl.endpoint.identification.algorithm", "");
     brokerProperties.put("sasl.enabled.mechanisms", "PLAIN, OAUTHBEARER");
     brokerProperties.put("sasl.mechanism.inter.broker.protocol", "PLAIN");
+    // brokerProperties.put("controller.listener.names","PLAINTEXT");
+    // brokerProperties.put("sasl.mechanism.controller.protocol","PLAIN");
     brokerProperties.put("listener.name.external.sasl.enabled.mechanisms", "OAUTHBEARER");
     brokerProperties.put("inter.broker.listener.name", "INTERNAL");
     // brokerProperties.put("listener.name.internal.sasl.enabled.mechanisms", "PLAIN");
@@ -184,18 +224,22 @@ public class WireMockInitializer
     brokerProperties.put(
         "sasl.oauthbearer.expected.audience", WireMockInitializer.BROKER_SASL_EXPECTED_AUD);
 
-    final var broker =
-        new EmbeddedKafkaZKBroker(
-            1,
-            false,
-            KafkaConsumerWithSSLAndBearerAuthIntegrationTest.STRING_TOPIC,
-            KafkaConsumerWithSSLAndBearerAuthIntegrationTest.MESSAGE_TOPIC);
-    broker.zkPort(Integer.parseInt(WireMockInitializer.BROKER_ZK_PORT));
     /*    final var broker =
+    new EmbeddedKafkaZKBroker(
+        1,
+        true,
+        KafkaConsumerWithSSLAndBearerAuthIntegrationTest.STRING_TOPIC,
+        KafkaConsumerWithSSLAndBearerAuthIntegrationTest.MESSAGE_TOPIC);*/
+    final var broker = new EmbeddedKafkaZKBroker(1);
+    broker.zkPort(Integer.parseInt(WireMockInitializer.BROKER_ZK_PORT));
+
+    // https://github.com/spring-projects/spring-kafka/issues/2916
+    /*        final var broker =
     new EmbeddedKafkaKraftBroker(
         1,1,
         KafkaConsumerWithSSLAndBearerAuthIntegrationTest.STRING_TOPIC,
         KafkaConsumerWithSSLAndBearerAuthIntegrationTest.MESSAGE_TOPIC);*/
+
     broker.kafkaPorts(Integer.parseInt(WireMockInitializer.BROKER_SSL_PORT));
     broker.brokerProperties(brokerProperties);
     // broker.afterPropertiesSet();
@@ -204,16 +248,15 @@ public class WireMockInitializer
 
   private static KafkaAdmin kafkaAdmin(EmbeddedKafkaBroker embeddedKafkaBroker) {
     Map<String, Object> configs = new HashMap<>();
-    configs.put(
-        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaBroker.getBrokersAsString());
+    configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + BROKER_SSL_PORT);
     configs.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
     configs.put("sasl.mechanism", "OAUTHBEARER");
     configs.put(
         "ssl.truststore.location", "src/test/resources/self-signed-certs/client-truststore2.p12");
     configs.put("ssl.truststore.password", "test@test.com");
-    configs.put(
-        "ssl.keystore.location", "src/test/resources/self-signed-certs/client-keystore2.p12");
-    configs.put("ssl.keystore.password", "test@test.com");
+    //    configs.put(
+    //        "ssl.keystore.location", "src/test/resources/self-signed-certs/client-keystore2.p12");
+    //    configs.put("ssl.keystore.password", "test@test.com");
     configs.put(
         "sasl.jaas.config",
         "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required "
@@ -232,10 +275,13 @@ public class WireMockInitializer
         "sasl.login.callback.handler.class",
         "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginCallbackHandler");
     configs.put("sasl.oauthbearer.token.endpoint.url", CONSUMER_OAUTH_TOKEN_ENDPOINT);
-    return new KafkaAdmin(configs);
+    KafkaAdmin kafkaAdmin = new KafkaAdmin(configs);
+    kafkaAdmin.setFatalIfBrokerNotAvailable(true);
+    // kafkaAdmin.afterSingletonsInstantiated();
+    return kafkaAdmin;
   }
 
   private static AdminClient adminClient(KafkaAdmin kafkaAdmin) {
-    return AdminClient.create(kafkaAdmin.getConfigurationProperties());
+    return KafkaAdminClient.create(kafkaAdmin.getConfigurationProperties());
   }
 }
